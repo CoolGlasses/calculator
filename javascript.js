969d7f241f0f6f74124ed749a1b2498b1910d944
Robin Andrea <robinandrea_2@yahoo.com>
2019-11-01 13:34:06 -0700

    added percent and root functions to javascript file

--- javascript.js
+++ javascript.js
@@ -14,6 +14,18 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+function percent(num) {
+    return num / 100;
+}
+
+function root(num) {
+    if (num < 0) {
+        return "Irrational Number!"
+    } else {
+        return Math.sqrt(num);
+    }
+}
+
 function operate(operator, num1, num2) {
     if (operator == "+"){
         add(num1, num2);
