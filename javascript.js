ecc816e8db5150786a4328779054b283290b15fb
Robin Andrea <robinandrea_2@yahoo.com>
2019-11-04 11:05:57 -0800

    add functionality to make addition, subtraction, multiplication, division, equals and clear, all work as expected

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
 let expressionArray = [];
 
-
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -51,97 +50,184 @@ function operate(operator, num1, num2) {
 
 
 
-function clearDisplay(displayValue) {
+function clearDisplay() {
     let displayBar = document.querySelector("#display");
     displayBar.textContent = "";
+    expressionArray = [];
 }
 
 function showDisplay(expressionArray) {
-    let displayBar = document.querySelector("#dispaly");
+    let displayBar = document.querySelector("#display");
+    displayBar.textContent = expressionArray.join("");
+}
+
+function evaluateExpression(finalExpressionArray) { //accepts an array like this:  [456, " + ", 789, " * ", 123]
+    let displayBar = document.querySelector("#display");
+    let answer = 0;
+    let finished = false;
+
+    while (!finished) { 
+        finished = true;
+        let indexOfMultiply = finalExpressionArray.indexOf(" * ");
+        let indexOfDivide = finalExpressionArray.indexOf(" / ");
+        let indexOfAdd = finalExpressionArray.indexOf(" + ");
+        let indexOfSubtract = finalExpressionArray.indexOf(" - ");
+
+        if (indexOfMultiply != -1) {
+            answer += finalExpressionArray[indexOfMultiply - 1] * finalExpressionArray[indexOfMultiply + 1];
+            finalExpressionArray.splice(indexOfMultiply - 1, 1);
+            finalExpressionArray.splice(indexOfMultiply - 1, 1);
+            finalExpressionArray.splice(indexOfMultiply - 1, 1);
+            finalExpressionArray.splice(indexOfMultiply, 1, answer);
+            finished = false;
+        } else if (indexOfDivide != -1) {
+            answer += finalExpressionArray[indexOfDivide - 1] / finalExpressionArray[indexOfDivide + 1];
+            finalExpressionArray.splice(indexOfDivide - 1, 1);
+            finalExpressionArray.splice(indexOfDivide - 1, 1);
+            finalExpressionArray.splice(indexOfDivide, 1, answer);
+            finished = false;
+        } else if (indexOfAdd != -1) {
+            answer += finalExpressionArray[indexOfAdd - 1] + finalExpressionArray[indexOfAdd + 1];
+            finalExpressionArray.splice(indexOfAdd - 1, 1);
+            finalExpressionArray.splice(indexOfAdd - 1, 1);
+            finalExpressionArray.splice(indexOfAdd, 1, answer);
+            finished = false;
+        } else if (indexOfSubtract != -1) {
+            answer += finalExpressionArray[indexOfSubtract - 1] - finalExpressionArray[indexOfSubtract + 1];
+            finalExpressionArray.splice(indexOfSubtract - 1, 1);
+            finalExpressionArray.splice(indexOfSubtract - 1, 1);
+            finalExpressionArray.splice(indexOfSubtract, 1, answer);
+            finished = false;
+        }
+    }
+    displayBar.textContent = answer;
+    expressionArray = [];
+}
+
+document.getElementById("equals").addEventListener("click", function () {
     let finalExpressionArray = [];
     let expression = "";
 
-    for (let i = 0; i < expressionArray.length; i++){
-        let isOperand = (expressionArray[i] == "+" || expressionArray[i] == "-" || expressionArray[i] == "/" || expressionArray[i] == "*");
+    for (let i = 0; i < expressionArray.length; i++) {
+        let isOperand = (expressionArray[i] == " + " || expressionArray[i] == " - " || expressionArray[i] == " / " || expressionArray[i] == " * ");
         if (isOperand == true && expression != "") {
-            finalExpressionArray.push(expression);
+            finalExpressionArray.push(parseInt(expression, 10));
             finalExpressionArray.push(expressionArray[i]);
             expression = "";
         } else if (i == expressionArray.length - 1) {
-            finalExpressionArray.push(expression);
+            expression += expressionArray[i];
+            finalExpressionArray.push(parseInt(expression, 10));
         } else {
             expression += expressionArray[i];
         }
     }
-    
-    displayBar.textContent = finalExpressionArray.join(" ");
-}
+
+    evaluateExpression(finalExpressionArray);
+});
 
 
 
-
-document.getElementById("0").addEventListener( function () {
+document.getElementById("0").addEventListener("click", function(){
     expressionArray.push("0");
     showDisplay(expressionArray);
 });
 
 
-document.getElementById("1").addEventListener(function () {
+document.getElementById("1").addEventListener("click", function(){
     expressionArray.push("1");
     showDisplay(expressionArray);
 });
 
-document.getElementById("2").addEventListener(function () {
+document.getElementById("2").addEventListener("click", function(){
     expressionArray.push("2");
     showDisplay(expressionArray);
 });
 
-document.getElementById("3").addEventListener(function () {
+document.getElementById("3").addEventListener("click", function(){
     expressionArray.push("3");
     showDisplay(expressionArray);
 });
 
-document.getElementById("4").addEventListener(function () {
+document.getElementById("4").addEventListener("click", function(){
     expressionArray.push("4");
     showDisplay(expressionArray);
 });
 
-document.getElementById("5").addEventListener(function () {
+document.getElementById("5").addEventListener("click", function(){
     expressionArray.push("5");
     showDisplay(expressionArray);
 });
 
-document.getElementById("6").addEventListener(function () {
+document.getElementById("6").addEventListener("click", function(){
     expressionArray.push("6");
     showDisplay(expressionArray);
 });
 
-document.getElementById("7").addEventListener(function () {
+document.getElementById("7").addEventListener("click", function(){
     expressionArray.push("7");
     showDisplay(expressionArray);
 });
 
-document.getElementById("8").addEventListener( function () {
+document.getElementById("8").addEventListener("click", function(){
     expressionArray.push("8");
     showDisplay(expressionArray);
 });
 
-document.getElementById("9").addEventListener(function () {
+document.getElementById("9").addEventListener("click", function(){
     expressionArray.push("9");
     showDisplay(expressionArray);
 });
 
+document.getElementById("add").addEventListener("click", function(){
+    if (expressionArray.length == 0) {
+        expressionArray.push("0");
+        expressionArray.push(" + ");
+        showDisplay(expressionArray);
+    } else {
+        expressionArray.push(" + ");
+        showDisplay(expressionArray);
+    }
+});
 
+document.getElementById("subtract").addEventListener("click", function(){
+    if (expressionArray.length == 0) {
+        expressionArray.push("0");
+        expressionArray.push(" - ");
+        showDisplay(expressionArray);
+    } else {
+        expressionArray.push(" - ");
+        showDisplay(expressionArray);
+    }
+});
 
+document.getElementById("multiply").addEventListener("click", function(){
+    if (expressionArray.length == 0) {
+        expressionArray.push("0");
+        expressionArray.push(" * ");
+        showDisplay(expressionArray);
+    } else {
+        expressionArray.push(" * ");
+        showDisplay(expressionArray);
+    }
+});
 
-document.getElementById("add").addEventListener();
-document.getElementById("subtract").addEventListener();
-document.getElementById("multiply").addEventListener();
-document.getElementById("divide").addEventListener();
-document.getElementById("equals").addEventListener();
-document.getElementById("clear").addEventListener();
-document.getElementById("delete").addEventListener();
-document.getElementById("root").addEventListener();
-document.getElementById("percent").addEventListener();
-document.getElementById("decimal").addEventListener();
+document.getElementById("divide").addEventListener("click", function(){
+    if (expressionArray.length == 0) {
+        expressionArray.push("0");
+        expressionArray.push(" / ");
+        showDisplay(expressionArray);
+    } else {
+        expressionArray.push(" / ");
+        showDisplay(expressionArray);
+    }
+});
+
+document.getElementById("clear").addEventListener("click", function(){
+   clearDisplay();
+});
+
+// document.getElementById("delete").addEventListener("click", function(){ });
+// document.getElementById("root").addEventListener("click", function(){ });
+// document.getElementById("percent").addEventListener("click", function(){ });
+// document.getElementById("decimal").addEventListener("click", function(){ });
 
